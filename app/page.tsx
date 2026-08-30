28adb01461c4c984efb66fd8cc856fcff982b661
unknown <Tadaimaaaa>
2026-08-30 18:40:51 +0700

    style: remove redundant section title for timeline

--- app/page.tsx
+++ app/page.tsx
@@ -54,7 +54,7 @@ export default function Home() {
         </div>
       </Section>
 
-      <Section id="timeline" title="TANDAI KALENDER ANDA">
+      <Section id="timeline">
         <Timeline />
       </Section>
 
